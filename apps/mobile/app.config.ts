af979b39811b5ca00c6fd15db5fd23ea812d23af
tiesen243 <ttien56906@gmail.com>
2025-09-05 10:35:33 +0700

    feat: add expo app

---
+++ apps/mobile/app.config.ts
@@ -0,0 +1,33 @@
+import type { ConfigContext, ExpoConfig } from 'expo/config'
+
+import packageJson from './package.json' with { type: 'json' }
+
+export default ({ config }: ConfigContext): ExpoConfig => ({
+  ...config,
+  name: 'attendify',
+  slug: 'attendify',
+  scheme: 'attendify',
+  version: packageJson.version,
+
+  newArchEnabled: true,
+  orientation: 'portrait',
+  userInterfaceStyle: 'automatic',
+  updates: {
+    fallbackToCacheTimeout: 0,
+  },
+
+  ios: {
+    bundleIdentifier: 'com.attendify.mobile',
+    supportsTablet: true,
+  },
+  android: {
+    package: 'com.attendify.mobile',
+    edgeToEdgeEnabled: true,
+  },
+
+  plugins: ['expo-router', 'expo-splash-screen'],
+  experiments: {
+    tsconfigPaths: true,
+    typedRoutes: true,
+  },
+})
---
+++ apps/mobile/app/_layout.tsx
@@ -0,0 +1,13 @@
+import 'react-native-reanimated'
+
+import { Stack } from 'expo-router'
+import { StatusBar } from 'expo-status-bar'
+
+export default function RootLayout() {
+  return (
+    <>
+      <Stack />
+      <StatusBar style='dark' />
+    </>
+  )
+}
